245a9b00364f0f18517316ff6698544b7ff97ee5
Lina Samuelsson <lina.sk.samuelsson@hotmail.com>
2026-03-24 08:52:00 +0100

    Add project files.

---
+++ Stretching/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Stretching
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
