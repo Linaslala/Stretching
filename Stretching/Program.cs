55a46d728134bd58480127c7c51fceb79f314559
Lina Samuelsson <lina.sk.samuelsson@hotmail.com>
2026-03-26 08:33:58 +0100

    Added the rest of the exercises in Program.cs

--- Stretching/Program.cs
+++ Stretching/Program.cs
@@ -21,6 +21,30 @@ namespace Stretching
 
             exerciseList.AddExercise(exercise3);
 
+            var exercise4 = new Exercise("Kneeling Lounge", "Lägg in instruktion", 10, 30);
+
+            exerciseList.AddExercise(exercise4);
+
+            var exercise5 = new Exercise("Full Squat", "Lägg in instruktion", 10, 30);
+
+            exerciseList.AddExercise(exercise5);
+
+            var exercise6 = new Exercise("Butterfly", "Lägg in instruktion", 10, 30);
+
+            exerciseList.AddExercise(exercise6);
+
+            var exercise7 = new Exercise("Lying Twist", "Lägg in instruktion", 10, 30);
+
+            exerciseList.AddExercise(exercise7);
+
+            var exercise8 = new Exercise("Backbend", "Lägg in instruktion", 10, 30);
+
+            exerciseList.AddExercise(exercise8);
+
+            var exercise9 = new Exercise("Underarm - Shoulder", "Lägg in instruktion", 10, 30);
+
+            exerciseList.AddExercise(exercise9);
+
             Counter.CountDown(exercise1);
         }
     }
