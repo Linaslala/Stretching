83e503a58a904815f59a5aea0cc20323b37d8de5
Lina Samuelsson <lina.sk.samuelsson@hotmail.com>
2026-04-01 14:36:06 +0200

    Lagt till instruktioner till övning 4, 5 & 6

--- Stretching/ExerciseData.cs
+++ Stretching/ExerciseData.cs
@@ -23,15 +23,26 @@ namespace Stretching
 
             exerciseList.AddExercise(exercise3);
 
-            var exercise4 = new Exercise("Kneeling Lounge", "Lägg in instruktion", 10, 30);
+            var exercise4 = new Exercise("Kneeling Lounge",
+                "Kneel on the ground and Place one foot infront of you, flat on the ground. " +
+                "Keep your back knee and toes on the ground.\r\n" +
+                "Squeeze your glutus and move your pelvis forward, stretching the rear leg. " +
+                "Keep your front shin and torso aligned.", 
+                10, 30);
 
             exerciseList.AddExercise(exercise4);
 
-            var exercise5 = new Exercise("Full Squat", "Lägg in instruktion", 10, 30);
+            var exercise5 = new Exercise("Full Squat",
+                "Squat down as far as you can, keeping your heels on the ground.\r\n " +
+                "Keep your arms inside your knees and press them outward.", 
+                10, 30);
 
             exerciseList.AddExercise(exercise5);
 
-            var exercise6 = new Exercise("Butterfly", "Lägg in instruktion", 10, 30);
+            var exercise6 = new Exercise("Butterfly", 
+                "Sit on the ground and bring the bottoms of your feet together infront of you. Hold your feet together with your hands and\r\n" +
+                "and pull them slightly towards you. Actively try to press your knees to the ground.", 
+                10, 30);
 
             exerciseList.AddExercise(exercise6);
 
